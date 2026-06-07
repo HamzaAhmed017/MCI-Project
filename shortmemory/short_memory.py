b0016b451f26bf73e490f097539d3a3abe27b6ee
Hamza Ahmed <hamza.ahmed@stud.uni-due.de>
2026-06-07 16:43:36 +0200

    add: Buttons fürs Starten und auswahl der Testart

--- shortmemory/short_memory.py
+++ shortmemory/short_memory.py
@@ -13,7 +13,31 @@ class ShortMemory(tk.Frame):
             text="Augenbewegungen",
             font=("Arial", 24)
         ).pack(pady=20)
-        
+
+        button_frame = tk.Frame(main_frame)
+        button_frame.pack(pady=20)
+
+        tk.Button(
+            button_frame,
+            text="Start",
+            font=("Arial",24)
+        ).pack(side="left", pady=20)
+
+        v = True
+        tk.Radiobutton(
+            button_frame,
+            text="Normale Darstellung",
+            variable=v,
+            value=bool
+        ).pack(side="left", pady=20)
+
+        tk.Radiobutton(
+            button_frame,
+            text="Chunkbasiert",
+            variable=v,
+            value= bool
+        ).pack(side="left", pady=20)
+
         tk.Button(
             self,
             text="Zurück zum Menü",
